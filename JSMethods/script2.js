747cc8e9ca8c2a69ff0d5eb60fbbc83611028176
munnazarkhan <heavenkhan30@gmail.com>
2026-05-01 19:48:25 +0530

    todays topic

---
+++ JSMethods/script2.js
@@ -0,0 +1,29 @@
+const abc = function () {
+    console.log ("Hello heaven");
+};
+
+console.log(abc);
+
+abc();
+
+let add = function(a, b){
+    return a + b;
+};
+console.log(add);
+
+console.log(add(4, 6));
+
+console.log(add());
+
+
+
+const arrowfun = () => {
+    console.log("I am Arrow function")
+};
+arrowfun();
+
+const addarrow = (a, b) => a + b; {
+     
+};
+
+console.log(addarrow(5,6));
