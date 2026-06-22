63914fab0f3752a4f990f8efbfaf72bd0e918419
munnazarkhan <heavenkhan30@gmail.com>
2026-06-22 20:23:49 +0530

    router and cantrollers

---
+++ Backend/FirstBackend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const LoginUser = (req,res)=>{
+    res.json({message:"Login Successfull from Controller"})
+};
+export const LogoutUser = (req,res)=>{
+    res.json({message:"Logout Successfull from Controller"})
+};
+export const RegisterUser = (req,res)=>{
+    res.json({message:"Register Successfull from Controller"})
+};
---
+++ Backend/FirstBackend/src/routers/auth.route.js
@@ -0,0 +1,14 @@
+import express from'express';
+import {
+    LoginUser,
+    LogoutUser,
+    RegisterUser
+} from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/login",LoginUser);
+router.post("/register", LogoutUser);
+router.get("/logout", RegisterUser);
+
+export default router;
