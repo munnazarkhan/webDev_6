c5aff64b668e07b1aad7dc7a1493bc81f4cea5ca
munnazarkhan <heavenkhan30@gmail.com>
2026-06-26 20:25:26 +0530

    login

--- Backend/FirstBackend/src/controllers/auth.controller.js
+++ Backend/FirstBackend/src/controllers/auth.controller.js
@@ -1,18 +1,20 @@
 import User from "../models/user.model.js";
 
-export const RegisterUser = async (req, res) => {
+export const RegisterUser = async (req, res, next) => {
   try {
     const { fullName, email, password, phone, gender, dob } = req.body;
 
     if (!fullName || !email || !password || !phone || !gender || !dob) {
-      res.status(400).json({ message: "All Feilds Required" });
-      return;
+      const error = new Error("All fields Required");
+      error.statusCode = 400;
+      return next(error);
     }
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      res.status(409).json({ message: "Email Already Registered" });
-      return;
+      const error = new Error("Email already registred");
+      error.statusCode = 409;
+      return next(error);
     }
 
     const photoUrl = `https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}`;
@@ -34,12 +36,43 @@ export const RegisterUser = async (req, res) => {
 
     res.status(201).json({ message: "User Created Successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
+    console.log(error.message);
+    next();
   }
 };
 
-export const LoginUser = (req, res) => {
-  res.json({ message: "Login Successfull from Controller" });
+export const LoginUser = async(req, res, next) => {
+  try{
+    const{email,password}= req.body;
+
+    if (!email ||!password){
+      const error = new Error("All field Required");
+      error.statusCode = 400;
+      return next(error);
+    }
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      const error = new Error("Email not registred");
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    if(password!== existingUser.password){
+      const error = new Error("Incorrect Password");
+      error.statusCode = 401;
+      return next(error);
+    }
+    res.status(200).json({
+      message:"Welcome Back",
+      data: existingUser,
+    });
+
+  }
+  catch (error){
+    console.log(error.message);
+    next();
+  }
+
 };
 
 export const LogoutUser = (req, res) => {
